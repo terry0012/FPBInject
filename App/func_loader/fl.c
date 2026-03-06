8816fb1c4c59b0a5ac14279788e5e06de0b2914f
VIFEX <vifextech@foxmail.com>
2026-03-06 16:18:16 +0800

    chore(func_loader): remove FL_MAX_ARGC define

--- App/func_loader/fl.c
+++ App/func_loader/fl.c
@@ -52,10 +52,6 @@
 #include <string.h>
 #include <limits.h>
 
-#ifndef FL_MAX_ARGC
-#define FL_MAX_ARGC 32
-#endif
-
 static uint16_t calc_crc16_base(uint16_t crc, const void* data, size_t len) {
     static const uint16_t s_crc16_table[256] = {
         0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50A5, 0x60C6, 0x70E7, 0x8108, 0x9129, 0xA14A, 0xB16B, 0xC18C, 0xD1AD,
